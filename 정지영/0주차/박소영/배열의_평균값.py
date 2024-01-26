86105fd356f11b17df406475a16c7e2450af0bc1
soieu <102034740+soieu@users.noreply.github.com>
2024-01-26 22:02:57 +0900

    Update

--- 정지영/0주차/박소영/배열의_평균값.py
+++
@@ -1,6 +0,0 @@
-def solution(numbers):
-    sum = 0 
-    for i in numbers:
-        sum += i 
-    answer = sum / len(numbers)
-    return answer
--- 정지영/0주차/정지영/배열의 평균값.py
+++
@@ -1,6 +0,0 @@
-# import numpy as np
-
-def solution(numbers):
-    return (sum(numbers)/len(numbers))
-    # return (np.mean(numbers))
-    
